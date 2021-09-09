7cd619b5d43ba075fbcecbe6b48099b0827d8ef0
JessMotta <jessicalimamotta@gmail.com>
2021-09-09 16:36:17 -0300

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+// Carne - 400g por pessoa, se for + de 6h = 650g
+// Cerveja - 1200ml por pessoa, se for + de 6h = 2000ml
+//Refrigerante/agua - 1000ml por pessoa, se for + 6h = 1500ml
+
+// Crianças valem por 0,5 pessoa
+
+// ler os elementos de entrada
+let inputAdultos = document.getElementById("adultos");
+let inputCriancas = document.getElementById("criancas");
+let inputDuracao = document.getElementById("duracao");
+
+let resultado = document.getElementById("resultado");
+
+function calcular(){
+    // pegar os valores dentro das caixas de texto
+    let  adultos = inputAdultos.value;
+    let  criancas = inputCriancas.value;
+
+}
+
