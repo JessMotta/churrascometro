96b8176e1a0e553ae2a87692122c028625ebc981
JessMotta <jessicalimamotta@gmail.com>
2021-09-09 17:06:05 -0300

    add script

--- scripts/script.js
+++ scripts/script.js
@@ -12,9 +12,54 @@ let inputDuracao = document.getElementById("duracao");
 let resultado = document.getElementById("resultado");
 
 function calcular(){
-    // pegar os valores dentro das caixas de texto
-    let  adultos = inputAdultos.value;
-    let  criancas = inputCriancas.value;
+    // acessar os valores dentro das caixas de texto
+    let adultos = inputAdultos.value;
+    let criancas = inputCriancas.value;
+    let duracao = inputDuracao.value;
+
+    // variaveis recebem o retorno das funções
+
+    let carne = carnePP(duracao);
+    let cerveja = cervejaPP(duracao);
+    let bebidas = bebidasPP(duracao);
+
+
+    let qtdTotalCarne = carne * adultos + (carne / 2 * criancas);
+    let qtdTotalCerveja = cerveja * adultos;
+    let qtdTotalBebidas = bebidas * adultos + (bebidas / 2 * criancas);
+
+
+    resultado.innerHTML = `<p>${qtdTotalCarne / 1000}kg de carne</p>`
+    resultado.innerHTML += `<p>${Math.ceil(qtdTotalCerveja / 355)} latas de cerveja</p>`
+    resultado.innerHTML += `<p>${Math.ceil(qtdTotalBebidas / 2000)} garrafas de bebidas</p>`
 
 }
+
+    function carnePP(duracao){
+        if(duracao >= 6){
+            return 650;
+        }
+        else{
+            return 400;
+        }
+    }
+
+    function cervejaPP(duracao){
+        if(duracao >= 6){
+            return 2000;
+        }
+        else{
+            return 1200;
+        }
+    } 
+
+    function bebidasPP(duracao){
+        if(duracao >= 6){
+            return 1500;
+        }
+        else{
+            return 1000;
+        }
+    }
+
 
